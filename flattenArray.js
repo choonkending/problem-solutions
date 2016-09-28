4834f7bd3fdeb74d1790e72c5021461ba6c3dc61
Ken Ding <aenkil@gmail.com>
2016-09-29 07:23:25 +1000

    Simplify flatten function

--- flattenArray.js
+++ flattenArray.js
@@ -1,10 +1,16 @@
 /*
  * Flatten array
  */
+const testOne = [1, 2, 3, 4];
+const testTwo = [1, 2, [3, 4], 5];
+const testThree = [1, [2, [3], 4]];
 
 const flattenArray = array => array.reduce((acc, cur) => {
   if (Array.isArray(cur)) {
-    return [...acc, ...flattenArray(cur)];
+    return acc.concat(flattenArray(cur));
   }
-  return [...acc, cur];
+  return acc.concat(cur);
 }, []);
+
+
+
